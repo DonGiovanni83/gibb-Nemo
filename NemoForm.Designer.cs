3cedf8fa37f2133cdbe26a7805ee8ac8a29e26b0
vmadmin <vmadmin@BMWP1>
2020-12-14 15:51:25 +0100

    Info Panel initialize

---
+++ NemoForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Nemo
+{
+    partial class NemoForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInfo = new System.Windows.Forms.Label();
+            this.InfoPanel = new System.Windows.Forms.Panel();
+            this.InfoPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtInfo.AutoSize = true;
+            this.txtInfo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtInfo.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.txtInfo.Location = new System.Drawing.Point(347, 42);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(77, 21);
+            this.txtInfo.TabIndex = 0;
+            this.txtInfo.Text = "Info Text";
+            // 
+            // InfoPanel
+            // 
+            this.InfoPanel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.InfoPanel.Controls.Add(this.txtInfo);
+            this.InfoPanel.Location = new System.Drawing.Point(12, 12);
+            this.InfoPanel.Name = "InfoPanel";
+            this.InfoPanel.Size = new System.Drawing.Size(776, 100);
+            this.InfoPanel.TabIndex = 1;
+            // 
+            // NemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.InfoPanel);
+            this.Name = "NemoForm";
+            this.Text = "Nemo";
+            this.InfoPanel.ResumeLayout(false);
+            this.InfoPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtInfo;
+        private System.Windows.Forms.Panel InfoPanel;
+    }
+}
+
--- NemoForm.cs
+++ NemoForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Nemo
 {
-    public partial class Form1 : Form
+    public partial class NemoForm : Form
     {
-        public Form1()
+        public NemoForm()
         {
             InitializeComponent();
         }
